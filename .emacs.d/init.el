12a4938b0f894b6ed52a1724ba58083c9c0f293c
ekr <cr.lonhard@gmail.com>
2016-06-23 11:27:12 +0300

    emacs config update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,21 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(setq inhibit-startup-message t)
+(setq column-number-mode t)
+(set-face-attribute 'default nil :height 80)
 
 ;; fix path
 (setenv "PATH" (concat (getenv "PATH") ":/home/ekr/local/bin/"))
 (setq exec-path (append exec-path '("/home/ekr/local/bin/")))
 
 ;;(require 'ggtags)
+
+;;
+;; magit configuration
+;; 
+;; (require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
 
 (add-hook 'c-mode-common-hook
           (lambda ()
@@ -34,8 +44,8 @@
  ;; If there is more than one, they won't work right.
  )
 
-(add-to-list 'load-path "/home/ekr/.opam/system/share/emacs/site-lisp/")
-(load "/home/ekr/.opam/system/share/emacs/site-lisp/tuareg-site-file")
+;;(add-to-list 'load-path "/home/ekr/.opam/system/share/emacs/site-lisp/")
+;;(load "/home/ekr/.opam/system/share/emacs/site-lisp/tuareg-site-file")
 (setq column-number-mode t)
 (setq line-number-mode t)
 (setq linum-mode t)
